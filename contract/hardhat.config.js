13c68d769eb0685ab1c4d9deb4eb86143336f4c7
Alishak003 <dopex@gmail.com>
2024-12-09 10:53:56 +0530

    json-rpc error needs to be debugged

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -6,13 +6,8 @@ module.exports = {
 
   // Configure networks
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545", // This is the default URL for Hardhat's local node
-      chainId: 1337,
-    },
     hardhat: {
       chainId: 31337, // Default chain ID for Hardhat network
-      persist: true,
     },
   },
 };
